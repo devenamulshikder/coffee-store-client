b9a74a338610df1fad8d42f520f51e199a835f9d
devenamulshikder <devenamulshikder@gmail.com>
2025-05-14 11:14:04 +0600

    coffee store client side setup okay

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className="raleway">this is navbar</div>;
+};
+
+export default Navbar;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='rancho text-5xl'>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
